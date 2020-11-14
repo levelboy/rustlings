296765f9f9d27b2ca404e4b9415c15180a07e3db
Adrian Nistor <adi@agilefreaks.com>
2020-11-14 16:51:59 +0200

    Complete functions

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,10 +1,9 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    let a: i32 = 1;
+    call_me(a);
 }
 
 fn call_me(num: i32) {
