bcbc8fadb984b27776fbd4ef76187c3e7c4ba0a2
Adrian Nistor <adi@agilefreaks.com>
2020-11-19 09:20:00 +0200

    Complete quiz4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -4,8 +4,12 @@
 // - Macros
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
-
-// I AM NOT DONE
+#[macro_use]
+macro_rules! my_macro {
+    ($val:expr) => {
+        format!("Hello {}", $val)
+    };
+}
 
 #[cfg(test)]
 mod tests {
